a7ec8718d967a65e434dfedec261a725bfc7ecfc
JellyBrick <shlee1503@naver.com>
2021-06-16 23:52:56 +0900

    update: deps

--- src/constants/Configs.ts
+++ src/constants/Configs.ts
@@ -1,9 +1,9 @@
 import { ClientConfig } from 'node-kakao';
 
 export const CLIENT: Partial<ClientConfig> = {
-  version: '3.1.9',
-  appVersion: '3.1.9.2626',
-  xvcSeedList: ['JAYDEN', 'JAYMOND'],
+  version: '3.2.7',
+  appVersion: '3.2.7.2782',
+  xvcSeedList: ['KEPHA', 'HALEY'],
 };
 
 export default {
